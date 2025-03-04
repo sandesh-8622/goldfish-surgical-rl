a884d6587fcc71cfd8b2ba34e3d5a7d6f0551de5
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2025-03-04 21:14:00 +0000

    add basic App skeleton with placeholder content

---
+++ goldfish-site/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>goldfish</h1>
+      <p>surgical robot training environment</p>
+    </div>
+  )
+}
+
+export default App
