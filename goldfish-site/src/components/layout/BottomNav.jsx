d6b449b47352d9a6c5ae7ed0db6870d7a10d6679
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2025-03-13 22:30:00 +0000

    add BottomNav skeleton for mobile screens

---
+++ goldfish-site/src/components/layout/BottomNav.jsx
@@ -0,0 +1,13 @@
+export default function BottomNav() {
+  return (
+    <nav className="bottom-nav">
+      <ul>
+        <li>Overview</li>
+        <li>Problem</li>
+        <li>Reward</li>
+        <li>System</li>
+        <li>Research</li>
+      </ul>
+    </nav>
+  )
+}
