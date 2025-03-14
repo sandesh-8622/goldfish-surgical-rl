a57fedaba46a2f6457532e3b61f5fcf3550be8ad
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2025-03-14 10:15:00 +0000

    add react-router with placeholder routes for each view

--- goldfish-site/src/App.jsx
+++ goldfish-site/src/App.jsx
@@ -1,15 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Sidebar from './components/layout/Sidebar'
 import './App.css'
 
 function App() {
   return (
-    <div className="app">
-      <Sidebar />
-      <main className="content">
-        <h1>goldfish</h1>
-        <p>surgical robot training environment</p>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Sidebar />
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<div>overview</div>} />
+            <Route path="/problem" element={<div>problem</div>} />
+            <Route path="/reward" element={<div>reward</div>} />
+            <Route path="/system" element={<div>system</div>} />
+            <Route path="/research" element={<div>research</div>} />
+            <Route path="/docs" element={<div>docs</div>} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
 
