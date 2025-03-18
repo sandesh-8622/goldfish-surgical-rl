f37b7dab30d76ca628d733f4897c3a763527daa9
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2025-03-18 14:18:00 +0000

    add ProblemView skeleton

---
+++ goldfish-site/src/views/ProblemView.jsx
@@ -0,0 +1,14 @@
+import styles from './ProblemView.module.css'
+
+export default function ProblemView() {
+  return (
+    <section className={styles.problem}>
+      <h1>The problem</h1>
+      <p>
+        surgical robots are getting better at moving. they are not getting
+        better at deciding what to do. most reward functions reduce surgery
+        to a positioning task. surgery is not a positioning task.
+      </p>
+    </section>
+  )
+}
