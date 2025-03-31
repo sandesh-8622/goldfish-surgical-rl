552f7ab3b949fdd7ac433899e31e1330c401be6f
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2025-03-31 20:14:00 +0000

    add SystemView skeleton

---
+++ goldfish-site/src/views/SystemView.jsx
@@ -0,0 +1,13 @@
+import styles from './SystemView.module.css'
+
+export default function SystemView() {
+  return (
+    <section className={styles.system}>
+      <h1>The system</h1>
+      <p>
+        kelvin-voigt tissue mechanics. ppo policy. evidence logger. all stitched
+        together in a single training loop.
+      </p>
+    </section>
+  )
+}
