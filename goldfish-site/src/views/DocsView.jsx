7cdf618e831074c798c6a8e22370dbe750399de9
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2025-04-10 11:25:00 +0000

    add DocsView skeleton

---
+++ goldfish-site/src/views/DocsView.jsx
@@ -0,0 +1,10 @@
+import styles from './DocsView.module.css'
+
+export default function DocsView() {
+  return (
+    <section className={styles.docs}>
+      <h1>Docs</h1>
+      <p>everything you need to actually run the training environment.</p>
+    </section>
+  )
+}
