8dc77d003db90f108306212db7878a9e6c59f203
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2025-07-12 19:05:00 +0000

    add envs.py with LayeredTissueSimulator skeleton

---
+++ training-modules/batch-1/goldfish/envs.py
@@ -0,0 +1,12 @@
+"""
+goldfish.envs
+
+needle insertion environment with kelvin-voigt soft tissue mechanics.
+"""
+
+import numpy as np
+
+
+class LayeredTissueSimulator:
+    """layered soft tissue with kelvin-voigt viscoelastic response."""
+    pass
