7e551c7c41e5b3a5fcc3147c7812c85955417b5f
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2025-07-22 12:33:00 +0000

    add PPO training loop with MlpPolicy and default hyperparameters

--- training-modules/batch-1/goldfish/agents.py
+++ training-modules/batch-1/goldfish/agents.py
@@ -15,3 +15,23 @@ class GoldfishPPOTrainer:
         self.env = env
         self.total_timesteps = total_timesteps
         self.output_dir = output_dir
+        self.model = None
+
+    def train(self):
+        from stable_baselines3 import PPO
+        from stable_baselines3.common.vec_env import DummyVecEnv
+
+        vec_env = DummyVecEnv([lambda: self.env])
+        self.model = PPO(
+            "MlpPolicy",
+            vec_env,
+            verbose=1,
+            n_steps=2048,
+            batch_size=64,
+            n_epochs=10,
+            learning_rate=3e-4,
+            gamma=0.99,
+            gae_lambda=0.95,
+        )
+        self.model.learn(total_timesteps=self.total_timesteps)
+        return self.model
