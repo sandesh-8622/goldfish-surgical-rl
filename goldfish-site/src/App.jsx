4dde9125dc5ed2e6e0f3d6e41b6b093ebcf10a4f
Sandesh Bhandari <110735997+sandesh-8622@users.noreply.github.com>
2026-05-16 17:02:10 -0500

    Refactor App component and update routes

--- goldfish-site/src/App.jsx
+++ goldfish-site/src/App.jsx
@@ -1,33 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Sidebar from './components/layout/Sidebar'
-import BottomNav from './components/layout/BottomNav'
-import OverviewView from './views/OverviewView'
-import ProblemView from './views/ProblemView'
-import RewardView from './views/RewardView'
-import SystemView from './views/SystemView'
-import ResearchView from './views/ResearchView'
-import DocsView from './views/DocsView'
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Sidebar from './components/layout/Sidebar.jsx';
+import BottomNav from './components/layout/BottomNav.jsx';
+import OverviewView   from './views/OverviewView.jsx';
+import ProblemView    from './views/ProblemView.jsx';
+import SystemView     from './views/SystemView.jsx';
+import ResearchView   from './views/ResearchView.jsx';
+import DocsView       from './views/DocsView.jsx';
+import RewardView     from './views/RewardView.jsx';
+import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <div className="app">
+    <div className="app-page">
+      <aside className="app-sidebar">
         <Sidebar />
-        <main className="content">
-          <Routes>
-            <Route path="/" element={<OverviewView />} />
-            <Route path="/problem" element={<ProblemView />} />
-            <Route path="/reward" element={<RewardView />} />
-            <Route path="/system" element={<SystemView />} />
-            <Route path="/research" element={<ResearchView />} />
-            <Route path="/docs" element={<DocsView />} />
-          </Routes>
-        </main>
-        <BottomNav />
-      </div>
-    </BrowserRouter>
-  )
-}
+      </aside>
 
-export default App
+      <div className="app-content">
+        <div className="app-panel">
+          <Routes>
+            <Route path="/"          element={<Navigate to="/overview" replace />} />
+            <Route path="/overview"  element={<OverviewView />} />
+            <Route path="/problem"   element={<ProblemView />} />
+            <Route path="/system"    element={<SystemView />} />
+            <Route path="/research"  element={<ResearchView />} />
+            <Route path="/docs"      element={<DocsView />} />
+            <Route path="/reward"    element={<RewardView />} />
+          </Routes>
+        </div>
+      </div>
+
+      <BottomNav />
+    </div>
+  );
+}
